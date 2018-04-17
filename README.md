553a191643a147420d81bbeb3bcfec91b559d121
huayun.mc <huayun.mc@alibaba-inc.com>
2018-04-17 23:21:52 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# MyWorld
+
+这是我的新博客
+
+## tmp
+
+- [编辑器](https://github.com/securingsincity/react-ace)
--- app/pages/manager/container/newArticle.js
+++ app/pages/manager/container/newArticle.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/markdown';
+import 'brace/theme/textmate';
 
 export default class NewArticle extends React.Component {
   render () {
     return (
-      <h1>NewArticle</h1>
+      <div className="new-article-panel">
+        <div style={{width:'300px', height:'300px'}}>
+          <AceEditor
+            mode="markdown"
+            theme="textmate"
+            name="blah2"
+            fontSize={14}
+            highlightActiveLine={true}
+            showGutter={false}
+            tabSize={2}
+            value={`var a = 5;
+            var b = 6;`}
+            width="100%"
+            height="100%"
+            enableLiveAutocompletion={true}
+            style={{lineHeight: '1.6em'}} />
+        </div>
+      </div>
     )
   }
 }
