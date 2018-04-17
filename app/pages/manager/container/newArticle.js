005a52784d4822a1db38f5dfb7c299c03b0d184c
huayun.mc <huayun.mc@alibaba-inc.com>
2018-04-17 22:25:30 +0800

    update

---
+++ app/pages/manager/container/newArticle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class NewArticle extends React.Component {
+  render () {
+    return (
+      <h1>NewArticle</h1>
+    )
+  }
+}
--- app/pages/manager/index.js
+++ app/pages/manager/index.js
@@ -5,6 +5,7 @@ import { HashRouter, Route } from 'react-router-dom'
 import Container from './container/index';
 import DashBoard from './container/dashboard';
 import Article from './container/article';
+import NewArticle from './container/newArticle';
 
 ReactDOM.render(
   <HashRouter>
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Route exact path="/" component={DashBoard}></Route>
       <Route path="/dashboard" component={DashBoard}></Route>
       <Route path="/article" component={Article}></Route>
+      <Route path="/newArticle" component={NewArticle}></Route>
     </Container>
   </HashRouter>
   , document.getElementById('app'));
